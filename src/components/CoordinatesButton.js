ed2aff95e83523b67624b679dfa3704b0f0c99c0
edwin rivera <waehake@gmail.com>
2018-01-30 03:20:16 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,12 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onReceiveCoordinates} />
+      </div>
+    );
+  }
+}
